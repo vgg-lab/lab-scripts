cdb491d0d35a75225053acd3846d7531373765ba
Igor Janos <ijanos84@gmail.com>
2022-12-17 16:00:50 +0100

    Este fix na ldap.conf

--- compute/ldap/install-ldapclient.sh
+++ compute/ldap/install-ldapclient.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Resolve public SSH keys from LDAP
-TARGET_FILE=/usr/local/bin/ldap-sshkey.sh
-if [ ! -f "${TARGET_FILE}"]; then
-    echo "Installing LDAP-SSHKEY script."
-    sudo cp ldap-sshkey.sh ${TARGET_FILE}
-    sudo chown root:users ${TARGET_FILE}
-fi
 
 sudo apt-get install -y libnss-ldap libpam-ldap ldap-utils
 sudo apt-get install -y nscd
@@ -15,3 +8,12 @@ sudo cp nsswitch.conf /etc/nsswitch.conf
 sudo cp common-password /etc/pam.d/common-password
 sudo cp common-session /etc/pam.d/comon-session
 
+
+# Resolve public SSH keys from LDAP
+TARGET_FILE=/usr/local/bin/ldap-sshkey.sh
+if [ ! -f "${TARGET_FILE}"]; then
+    echo "Installing LDAP-SSHKEY script."
+    sudo cp ldap-sshkey.sh ${TARGET_FILE}
+    sudo chown root:users ${TARGET_FILE}
+fi
+sudo cp ldap.conf /etc/ldap/ldap.conf
