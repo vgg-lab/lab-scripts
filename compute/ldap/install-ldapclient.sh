6b1c6ca98309c9fe50388a93e6d76385ae537cf8
Igor Janos <ijanos84@gmail.com>
2022-12-17 15:57:21 +0100

    LDAP instalacia

---
+++ compute/ldap/install-ldapclient.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Resolve public SSH keys from LDAP
+TARGET_FILE=/usr/local/bin/ldap-sshkey.sh
+if [ ! -f "${TARGET_FILE}"]; then
+    echo "Installing LDAP-SSHKEY script."
+    sudo cp ldap-sshkey.sh ${TARGET_FILE}
+    sudo chown root:users ${TARGET_FILE}
+fi
+
+sudo apt-get install -y libnss-ldap libpam-ldap ldap-utils
+sudo apt-get install -y nscd
+
+sudo cp nsswitch.conf /etc/nsswitch.conf
+sudo cp common-password /etc/pam.d/common-password
+sudo cp common-session /etc/pam.d/comon-session
+
