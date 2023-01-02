d9d24ed082a5fa4ae08a9d6296a70747732cf62a
Igor Janos <ijanos84@gmail.com>
2023-01-02 12:04:03 +0000

    Small fixes

--- compute/docker/install-docker.sh
+++ compute/docker/install-docker.sh
@@ -10,6 +10,13 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 rm get-docker.sh
 
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+            
 # 2. install rootless docker stuff
 sudo apt-get update
 sudo apt-get install -y nvidia-container-toolkit
--- compute/ldap/install-ldapclient.sh
+++ compute/ldap/install-ldapclient.sh
@@ -6,12 +6,12 @@ sudo apt-get install -y nscd
 
 sudo cp nsswitch.conf /etc/nsswitch.conf
 sudo cp common-password /etc/pam.d/common-password
-sudo cp common-session /etc/pam.d/comon-session
+sudo cp common-session /etc/pam.d/common-session
 
 
 # Resolve public SSH keys from LDAP
 TARGET_FILE=/usr/local/bin/ldap-sshkey.sh
-if [ ! -f "${TARGET_FILE}"]; then
+if [ ! -f "${TARGET_FILE}" ]; then
     echo "Installing LDAP-SSHKEY script."
     sudo cp ldap-sshkey.sh ${TARGET_FILE}
     sudo chown root:users ${TARGET_FILE}
